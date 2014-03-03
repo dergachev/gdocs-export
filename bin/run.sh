0868b8f115a43993473b6696a3bb0dd21408fd7d
Alex Dergachev <alex@evolvingweb.ca>
2014-03-03 18:28:21 -0500

    template-ew.tex now uses memoir class; refactoring

--- bin/run.sh
+++ bin/run.sh
@@ -1,19 +1,54 @@
-#!/bin/bash
+#!/bin/bash -x
 
 die() {
   echo "Usage: run.sh INPUTFILE.html"
   exit 1
 }
+
+FILE=$1
+FILE=${FILE//[[:space:]]/}  # remove spaces
+FILE=${FILE##*/}  # remove path (keeps only filename)
+FILE=${FILE%%.html}   # remove extension
+OUTPUT=build/"$FILE"
+
 test -e "$1" || die
+mkdir -p $OUTPUT
 
-lib/pandoc-preprocess.rb $1 \
-  | pandoc -f html -t json  \
-  | ./lib/pandoc-filter.py \
-  | pandoc -f json -t latex --template assets/template.tex \
-  > build/out.tex
+cp "$1" $OUTPUT/in.html
 
-# To add TOC:
-#   | pandoc -f json -t latex --template assets/template.tex -V toc:1 -V toc-depth:3 \
+# run the HTML preprocessor
+bundle exec ruby lib/pandoc-preprocess.rb $OUTPUT/in.html > $OUTPUT/preprocessed.html
 
-echo "Created build/out.tex, making build/out.pdf..."
-( cd build/ ; pdflatex out.tex)
+# run pandoc
+pandoc $OUTPUT/preprocessed.html -t json > $OUTPUT/pre.json
+
+# run the custom filter
+cat $OUTPUT/pre.json | ./lib/pandoc-filter.py > $OUTPUT/post.json
+
+# run pandoc
+#PANDOC_ARGS="--template assets/template.tex"
+PANDOC_ARGS="--template assets/template-ew.tex"
+PANDOC_ARGS="$PANDOC_ARGS -V numbersections:true"
+PANDOC_ARGS="$PANDOC_ARGS -V graphics:true"
+# PANDOC_ARGS="$PANDOC_ARGS --chapters"
+# PANDOC_ARGS="$PANDOC_ARGS -V toc:true toc-depth:3"
+
+# create main latex document
+# cp $OUTPUT/template-ew.tex $OUTPUT/$FILE.tex
+# USED TO REQUIRE PANDOC TEMPLATING SYSTEM, NOW DOES NOT:
+pandoc $OUTPUT/post.json -t latex $PANDOC_ARGS > $OUTPUT/$FILE.tex
+
+# create just the metadata latex document
+pandoc $OUTPUT/post.json --no-wrap -t latex --template assets/template-metadata.tex > $OUTPUT/metadata.tex
+pandoc $OUTPUT/post.json --chapters --no-wrap -t latex > $OUTPUT/main.tex
+
+# must use -o with docx output format
+pandoc $OUTPUT/post.json -s -t docx -o $OUTPUT/$FILE.docx
+pandoc $OUTPUT/post.json -s -t rtf -o $OUTPUT/$FILE.rtf
+
+#prepare the assets
+cp assets/* $OUTPUT/
+
+# convert latex to PDF
+echo "Created $OUTPUT/$FILE.tex, making $OUTPUT/$FILE.pdf..."
+( cd $OUTPUT/ ; rubber --pdf $FILE)
